d7fb51f8dadf622ddd401abb47ecceea60fed1d2
frnckx <jakab.fruzsi@gmail.com>
2017-12-03 20:56:30 +0100

    #2

--- src/hu/homework02/jf/CalculatorService.java
+++ src/hu/homework02/jf/CalculatorService.java
@@ -3,7 +3,29 @@ package hu.homework02.jf;
 public class CalculatorService {
 /*Ha osztható maradék nélkül kettővel, akkor páros, ha nem akkor páratlan */
 
-        public static boolean isItEven (int num){
-            return num % 2 == 0;
+    public static boolean isItEven(int num) {
+        return num % 2 == 0;
+    }
+/*Második feladat megoldása:*/
+
+    public static float calculate(int x, int y, char operation) {
+        float result = 0;
+        switch (operation) {
+            case '+':
+                result = x + y;
+            break;
+            case '-':
+                result=x - y;
+            break;
+            case '*':
+                result=x * y;
+            break;
+            case '/':
+                result= (float)x / y;
+            break;
+            default:
+                System.out.println("error");
+        }
+        return result;
     }
 }
--- src/hu/homework02/jf/Main.java
+++ src/hu/homework02/jf/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
 System.out.println(CalculatorService.isItEven(5));
+System.out.println(CalculatorService.calculate(10,3,'/'));
     }
 }
